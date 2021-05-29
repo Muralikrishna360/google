b7ebbd09d4f3da512cde8f05f836d1a50c57b6a2
Muralikrishna360 <kmuralikrishna360@gmail.com>
2021-05-29 21:18:45 +0530

    alowed host

--- fly/settings.py
+++ fly/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5!c4t7u(fc^e1)1rk&k5a!-xq1ic+t*5kf-s2zw3re^-$d3*d9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['git.heroku.com/thawing-dusk-44885.git', '127.0.0.1']
 
 
 # Application definition
